bb1bd5c56343a63780debc70fcad473057ce6cfd
Alif Fathi <alifptn1234@gmail.com>
2026-01-12 03:42:26 +0800

    Refactor MongoDB scripts for clarity and order
    Reordered and renumbered aggregation pipeline queries for consistency, improved formatting for readability, and removed redundant or duplicate queries in aggregationPipeline.mongodb.js and sorting.mongodb.js. Added spacing and minor clarifications in indexing.mongodb.js for better code clarity.

--- indexing.mongodb.js
+++ indexing.mongodb.js
@@ -2,11 +2,15 @@ use("libraryDB");
 
 // 1. Single Field Index (name)
 // Creates an index on the name field in the users collection
+
 db.users.createIndex({ name: 1 })
+
 // Explaination : A single-field index on name in users helps quickly retrieve users in alphabetical order as the index already maintains sorted data for efficient sorting operations.
 
 // 2. Compound Index (userId and payment.status)
 // Creates a compound index on userId and payment.status fields in the fines collection
+
 db.fines.createIndex({ userId: 1, "payment.status": 1 })
+
 // Explaination : A compound index on userId and payment.status in fines allows efficient retrieval of unpaid fines for specific users as admin often need to check the unpaid fines of a user.
 
--- sorting.mongodb.js
+++ sorting.mongodb.js
@@ -3,12 +3,7 @@ use("libraryDB");
 // 1. Sort users by name in ascending order
 db.users.find().sort({ name: 1 })
 
-// 2. Sort books by title in ascending order
-db.books.find().sort({ title: 1 })
-
-// 3. Sort books by published year in descending order
+// 2. Sort books by published year in descending order
 db.books.find().sort({ "publisher.year": -1 })
 
-// 4. Sort fines by amount in descending order
-db.fines.find().sort({ amount: -1 })
 
